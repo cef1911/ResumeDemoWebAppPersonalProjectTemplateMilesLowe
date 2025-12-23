611a6a7ab1171f10327cc53d9c07d73d2f420a92
Christopher Enam Franklin <cef1911@hotmail.com>
2025-12-23 18:06:25 -0500

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,36 @@
+from collections import namedtuple
+import altair as alt
+import math
+import pandas as pd
+import streamlit as st
+import numpy as np
+import time
+from PIL import Image
+
+#import plotly as px
+#import plotly.figure_factory as ff
+
+"""
+# Welcome to Chris Franklin's StreamLit Web App for Deploying DataScience Projects!
+This is inspired from the StreamLit.io site.
+See the website for more info https://streamlit.io/
+
+Click the link to view Dennis Gresham-Chappelle Resume 
+
+https://acrobat.adobe.com/id/urn:aaid:sc:US:f6753253-82dc-4bb6-9dda-745896a2ab49
+
+"""
+
+
+#st.balloons()
+
+#st.snow()
+
+#import streamlit as st, random, time    
+
+# time.sleep(1)
+# coins = ['http://re-bol.com/heads.jpg', 'http://re-bol.com/tails.jpg']
+# coin = random.choice(coins)
+# st.image(coin)
+# if st.button('Flip'):
+#   st.experimental_rerun()
